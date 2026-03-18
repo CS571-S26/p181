e7f1315427aac3cace6131b1a01b41e9c5611b20
Om Gulati <66808489+Omg577@users.noreply.github.com>
2026-03-18 18:58:42 -0500

    rebuilt the project into the correct folder to fix deployment issue to github pages

--- MoodSpace/vite.config.js
+++ MoodSpace/vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/p181/',
   plugins: [react()],
   build: {
-    outDir: "docs"
-  }
+    outDir: '../docs',
+    emptyOutDir: true,
+  },
 })
